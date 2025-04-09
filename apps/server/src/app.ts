d8f7b930c13daeaf29cef829db44c71e08eda0c6
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-09 17:04:34 +0530

    ⚙️ server: connect the routes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -4,6 +4,9 @@ import { Server } from "socket.io";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import checkRoutes from "./middleware/checkRoute.middleware";
+import auth from "./routes/auth.route";
+import chat from "./routes/chat.route";
+import search from "./routes/search.route";
 
 const app = express();
 const server = createServer(app); // Create HTTP server
@@ -20,9 +23,14 @@ app.use(express.json());
 app.use(cookieParser()); //parse the cookies
 app.use(checkRoutes); // check all routes 
 
+//api routes
+app.use("/api/auth", auth);
+app.use("/api/chat", chat);
+app.use("/api/search", search)
+
 // Default Route
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Hello from chat-app server" });
+  res.status(200).json({ msg: "Hello from cma server" });
 });
 
 // Export both `app` and `server`
